02ea8fe273f8c9519a9a424e2a490639a3d782c8
Sanja <sanjafennec@gmail.com>
2022-11-01 12:19:09 +0100

    create about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+
+function about() {
+	return (
+		<div>
+			<Heading>This is the about page</Heading>
+		</div>
+	);
+}
+
+export default about;
